b23f7acc457c375ef718f1653fb5b9ad401b4cf4
Alvaro Sanchez Palomino <alvarosanpal95@gmail.com>
2018-11-04 21:59:13 +0100

    y daaaale vueltas al codigo

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return jsonify(status="Ok")
+	return jsonify(status="Ok")
 
 @app.route('/portada')                        
 def portada():                      
