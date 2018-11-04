7f539f78043e35b832cca27056783dbdd82fd1d8
Alvaro Sanchez Palomino <alvarosanpal95@gmail.com>
2018-11-04 20:58:49 +0100

    actualizacion

---
+++ app/test.py
@@ -0,0 +1,18 @@
+#contenido de test.py
+import pytest
+from app import calcularAlturaTabla , calcularAlturaEsquis , calcularPrecio
+
+def test_alturaTabla():
+	alturaTabla = calcularAlturaTabla(178,60)
+	if alturaTabla > 0:
+		pass
+
+def test_alturaEsquis():
+	alturaEsquis = calcularAlturaEsquis(178,"medio")
+	if alturaEsquis > 0:
+		pass
+
+def test_precio():
+	precio = calcularPrecio(4)
+	if precio > 0:
+		pass
